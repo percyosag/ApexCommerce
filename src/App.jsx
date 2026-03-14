53c386503a7b2840e2dbeafdf25301fa27031051
Percy Osunde <osundepo@my.centennialcollege.ca>
2026-03-14 15:12:55 -0400

    Add Footer component and Bootstrap layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+
 const App = () => {
-  return <h1>Welcome to My E-Commerce Store</h1>;
+  return (
+    <>
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to My E-Commerce Store</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
