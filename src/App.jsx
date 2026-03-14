7431e3212a23a452fba30229ca43957a14b840d6
Percy Osunde <osundepo@my.centennialcollege.ca>
2026-03-14 15:21:22 -0400

    Add basic Header component with icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Container } from "react-bootstrap";
+import Header from "./components/Header"; // Import Header
 import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
+      <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to My E-Commerce Store</h1>
+          <h1>Welcome to ApexCommerce</h1>
         </Container>
       </main>
       <Footer />
