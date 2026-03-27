e89ef01c9236f07c1054c2d4edbdf9a89f9acdf9
Percy Osunde <posunde@my.centennialcollege.ca>
2026-03-27 05:41:46 -0400

    Header and Footer created + Add custom Bootstrap theme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,9 @@ const Header = () => {
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <Navbar.Brand href="/">ApexCommerce</Navbar.Brand>
+
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
               <Nav.Link href="/cart">
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.min.css"; // Standard bootstrap for now
+//import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/styles/bootstrap.custom.css";
 import App from "./App.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
