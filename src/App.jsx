3107406e7bd24b10da709abd45828d2c204efa8c
Percy Osunde <posunde@my.centennialcollege.ca>
2026-03-27 06:41:06 -0400

    Add React Router and Home screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,45 @@
-import { Container, Row, Col } from "react-bootstrap";
+// import { Container, Row, Col } from "react-bootstrap";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import Product from "./components/Product";
+// import products from "./data/products";
+
+// function App() {
+//   return (
+//     <>
+//       <Header />
+//       <main className="py-3">
+//         <Container>
+//           <h1>Latest Products</h1>
+//           <Row>
+//             {products.map((product) => (
+//               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+//                 <Product product={product} />
+//               </Col>
+//             ))}
+//           </Row>
+//         </Container>
+//       </main>
+//       <Footer />
+//     </>
+//   );
+// }
+
+// export default App;
+
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Product from "./components/Product";
-import products from "./data/products";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
     <>
       <Header />
       <main className="py-3">
-        <Container>
-          <h1>Latest Products</h1>
-          <Row>
-            {products.map((product) => (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-              </Col>
-            ))}
-          </Row>
-        </Container>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+        </Routes>
       </main>
       <Footer />
     </>
